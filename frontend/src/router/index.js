03eda61cfec122fd807a71b34e3db6f9f076a6fd
jasminezhang <jzhang59@uw.edu>
2023-12-09 00:43:47 -0800

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,12 +12,17 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/feed',
       name: 'home',
       component: HomeView
     },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
     {
-      path: '/feed',
+      path: '/',
       name: 'feed',
       component: FeedView
     },
