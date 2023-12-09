027d798e7e5f46dec47ba3f188d0132c62c6a70e
jasminezhang <jzhang59@uw.edu>
2023-12-09 00:21:07 -0800

    update url

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,8 +5,7 @@
                 <div class="menu-left">
                     <a href="#" class="text-xl">MusicMatch</a> 
                 </div>
-                <!-- v-if="userStore.user.isAuthenticated" -->
-                <div class="menu-center flex space-x-12"> 
+                <div class="menu-center flex space-x-12" v-if="userStore.user.isAuthenticated"> 
                     <RouterLink to="/feed">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -26,9 +25,8 @@
                     </RouterLink>
                 </div>
 
-                <!-- v-if="userStore.user.isAuthenticated && userStore.user.id" -->
                 <div class="menu-right">
-                    <template>
+                    <template v-if="userStore.user.isAuthenticated && userStore.user.id">
                         <RouterLink :to="{name: 'profile', params:{'id': userStore.user.id}}">
                             <img :src="'/头像.jpeg'" class="w-12 rounded-full"> 
                         </RouterLink>
